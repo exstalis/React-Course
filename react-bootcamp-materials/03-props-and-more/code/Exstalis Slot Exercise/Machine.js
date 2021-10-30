fcd64f2de9580053a28f22e2fa737e786119b22c
elif ece <exstalis@gmail.com>
2021-10-30 13:51:07 -0400

    Intro to styling with React

--- react-bootcamp-materials/03-props-and-more/code/Exstalis Slot Exercise/Machine.js
+++ react-bootcamp-materials/03-props-and-more/code/Exstalis Slot Exercise/Machine.js
@@ -10,27 +10,23 @@ function getRndIndex () {
 
 class Machine extends React.Component {
 
+  // const titleColor = {fonSize:'52px', color:'lilac'};
 
   render (){
 
     console.log(randomHeartArray2)
     let msg;
     let randomHeartArray2 = getRndIndex();
-      if (randomHeartArray2[0] == randomHeartArray2 [1] == randomHeartArray2 [2]) {
-        msg = this.props.winner
-      }
-      else {
-        msg = this.props.loser
-      }
+    const winner = randomHeartArray2[0] == randomHeartArray2[1] && randomHeartArray2[1] == randomHeartArray2 [2];
+    const exstalisTitleStyle = {fontSize:'52px'};
     return (
-      <div>
-        <h2>This is Exstalis' Slot Machine</h2>
-
+      <div className= "Machine">
+        <h2 style= {exstalisTitleStyle}>This is Exstalis' Slot Machine</h2>
         <p>{this.props.slotArray[randomHeartArray2[0]]},
           {this.props.slotArray[randomHeartArray2[1]]},
           {this.props.slotArray[randomHeartArray2[2]]}
         </p>
-        <p>{msg}</p>
+        <p className={ winner ? 'Winner-Style' : 'Loser-Style'}>{winner ? this.props.winner : this.props.loser}</p>
       </div>
     )
   }
