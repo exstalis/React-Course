d0627112d93e726e2936406590ffe8a3ed36bbd4
elif ece <exstalis@gmail.com>
2021-10-29 20:22:26 -0400

    Hello to Props cont

---
+++ react-bootcamp-materials/03-props-and-more/code/02_Hello_More_Props/index.js
@@ -0,0 +1,17 @@
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Hello
+					to="Atomsk"
+					from="Exstallis"
+					bigBangs={11}
+					myArray = {[1,2,3,5,8,13]}
+					img="galaxy.jpg"
+				/>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
